ca142db121ec60243056fad36dece798018ddf8c
hesse <hesse@DESKTOP-O13SV50>
2022-08-08 09:10:44 +0200

    Projektdateien hinzufügen.

---
+++ Voice_Bot_IT51/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace Voice_Bot_IT51
+{
+    partial class IT51
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbl_date = new System.Windows.Forms.Label();
+            this.lbl_time = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label1.Location = new System.Drawing.Point(48, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(202, 91);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "IT51";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(42, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 16);
+            this.label2.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 224);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "label3";
+            // 
+            // lbl_date
+            // 
+            this.lbl_date.AutoSize = true;
+            this.lbl_date.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_date.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lbl_date.Location = new System.Drawing.Point(29, 146);
+            this.lbl_date.Name = "lbl_date";
+            this.lbl_date.Size = new System.Drawing.Size(183, 18);
+            this.lbl_date.TabIndex = 3;
+            this.lbl_date.Text = "Date:_______________";
+            // 
+            // lbl_time
+            // 
+            this.lbl_time.AutoSize = true;
+            this.lbl_time.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_time.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lbl_time.Location = new System.Drawing.Point(94, 126);
+            this.lbl_time.Name = "lbl_time";
+            this.lbl_time.Size = new System.Drawing.Size(226, 20);
+            this.lbl_time.TabIndex = 4;
+            this.lbl_time.Text = "Time:_________________";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // IT51
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(297, 318);
+            this.Controls.Add(this.lbl_time);
+            this.Controls.Add(this.lbl_date);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "IT51";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "IT51";
+            this.Load += new System.EventHandler(this.IT51_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbl_date;
+        private System.Windows.Forms.Label lbl_time;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Voice_Bot_IT51/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+using System.IO;
+using System.Threading;
+
+namespace Voice_Bot_IT51
+{
+    public partial class IT51 : Form
+    {
+        SpeechRecognitionEngine h = new SpeechRecognitionEngine();
+        SpeechSynthesizer s = new SpeechSynthesizer();
+
+        Boolean hören = true;
+
+        public Boolean suche = false;
+        public IT51()
+        {
+            InitializeComponent();
+            timer1.Start();
+        }
+
+        private void IT51_Load(object sender, EventArgs e)
+        {
+            Choices commands = new Choices();
+            commands.Add(File.ReadAllLines(@"C:\Voice_Bot\commands.txt")); //@"C:\Voice_Bot\search.txt"
+            commands.Add(File.ReadAllLines(@"C:\Voice_Bot\search.txt"));
+            GrammarBuilder grbuilder = new GrammarBuilder();
+            grbuilder.Append(commands);
+      
+            Grammar grammar = new Grammar(grbuilder);
+
+            h.LoadGrammar(grammar);
+            h.SetInputToDefaultAudioDevice();
+            h.SpeechRecognized += recEngine_SpeechRecognized;
+
+            h.RecognizeAsync(RecognizeMode.Multiple);
+            s.SelectVoiceByHints(VoiceGender.Female, VoiceAge.Adult);
+            s.SpeakAsync("wie kann ich dir helfen?");
+        }
+
+        void recEngine_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+
+            Random r = new Random();
+            String[] hallo = new string[5] { "na du", "moin moin", "servus", "guten tag", "hi"};
+            
+            string target = "http://www.google.de"; //liste für webseiten erstellen
+            string target1 = "http://www.wetter.com";
+            string target3 = "steam://rungameid/292120"; //Steam Spiele URL mit der game ID
+            string modus = e.Result.Text;
+
+            if (modus == "sprich")
+            {
+                hören = true;
+            }
+            if (modus == "ruhe")
+            {
+                hören = false;
+            }
+
+            if (suche)
+            {
+                Process.Start("https://www.google.de/search?q=" + modus);
+                suche = false;
+            }
+
+            if (hören == true && suche == false)
+            {
+                switch (e.Result.Text)
+                {
+                    case "hallo":
+                        say(hallo[r.Next(5)]);
+                        break;
+                    case "guten morgen":
+                        say("tüdelüüüü");
+                        break;
+                    case "wie geht es dir":
+                        say("Mir geht es gut");
+                        break;
+                    case "wie spät ist es":
+                        say(DateTime.Now.ToString("HH:mm:ss tt"));
+                        break;
+                    case "welcher tag ist heute":
+                        say(DateTime.Now.ToString("dddd"));
+                        break;
+                    case "welches datum haben wir heute":
+                        say(DateTime.Now.ToString("d/MMMM/yyyy"));
+                        break;
+                    case "öffne wordpad":
+                        say("wird gemacht meister");
+                        Process.Start("wordpad");
+                        break;
+                    case "öffne google":
+                        Process.Start(target);
+                        break;
+                    case "wetter":
+                        Process.Start(target1);
+                        break;
+                    case "suche nach":
+                        suche = true;
+                        break;
+                 /* case "öffne spotify":
+                        Process.Start("");
+                        break;*/
+                    case "öffne steam":
+                        Process.Start("C:\\Program Files (x86)\\Steam\\Steam.exe");
+                        break;
+                    case "öffne ff":
+                        Process.Start(target3);
+                        break;
+                    case "exit":
+                        Application.Exit();
+                        break;
+                }
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lbl_time.Text = DateTime.Now.ToLongTimeString();
+            lbl_date.Text = DateTime.Now.ToLongDateString();
+        }
+
+        public void say(string h)
+        {
+            s.SpeakAsync(h);
+        }
+        
+    }
+}
